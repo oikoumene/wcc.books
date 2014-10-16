c75b795db9b216e1ff31af84ad2fe954a35d9ba8
Khairul Anwar (kulll) <anwarbaik88@gmail.com>
2014-10-16 11:17:53 +0800

    working listing for books and author

--- wcc/books/browser/authorlisting.py
+++ wcc/books/browser/authorlisting.py
@@ -1,10 +1,22 @@
 from five import grok
 from Products.CMFCore.interfaces import IContentish
+from Products.ATContentTypes.interfaces import IATFolder
+from plone.app.collection.interfaces import ICollection
+from wcc.books.content.author import IAuthorDataProvider
+
 
 grok.templatedir('templates')
 
+
 class AuthorListing(grok.View):
-    grok.context(IContentish)
+    grok.context(IATFolder)
     grok.name('authorlisting')
     grok.require('zope2.View')
     grok.template('authorlisting')
+
+    def authorprovider(self, item):
+        return IAuthorDataProvider(item.getObject())
+
+
+class AuthorListingCollection(AuthorListing):
+    grok.context(ICollection)
--- wcc/books/browser/bookslisting.py
+++ wcc/books/browser/bookslisting.py
@@ -1,10 +1,16 @@
 from five import grok
-from Products.CMFCore.interfaces import IContentish
+from plone.app.collection.interfaces import ICollection
+from Products.ATContentTypes.interfaces import IATFolder
 
 grok.templatedir('templates')
 
+
 class BooksListing(grok.View):
-    grok.context(IContentish)
+    grok.context(IATFolder)
     grok.name('bookslisting')
     grok.require('zope2.View')
     grok.template('bookslisting')
+
+
+class BooksListingCollection(BooksListing):
+    grok.context(ICollection)
